e1012aae228d2fcb430e4856d4c156f3d3784cae
elenaakisina <73779874+elenaakisina@users.noreply.github.com>
2020-11-23 16:31:48 +0300

    Create 10.3(2).js

---
+++ 10.3(2).js
@@ -0,0 +1,14 @@
+ var x;
+
+ x = null;
+if (typeof x == 'number')  
+{ console.log("x- число")} 
+ else 
+   if (typeof x == 'string')  
+{ console.log("x-  cтрока") }
+ else 
+   if (typeof x == 'boolean')  
+{ console.log("x-  логический оператор") }
+
+ else 
+ { console.log("Тип x не определён")}
